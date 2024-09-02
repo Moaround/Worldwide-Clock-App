da422275000b0826484f96ec7bc57c405ccbbdfd
Monique <mgggrol@gmail.com>
2024-09-02 21:57:42 +1000

    Add cities to the dropdown list

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ function updateCity(event) {
             "h:mm:ss"
           )}<small>${cityTime.format("A")}</small></div>
         </div>
-        <a href="/">All cities</a>
+        <a href="/ss">All cities</a>
   `;
 }
 
